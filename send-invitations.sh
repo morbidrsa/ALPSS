a7415c2c6e9a0761a8af0b527ecda546c4a45223
Johannes Thumshirn <jthumshirn@suse.de>
2018-07-27 10:41:21 +0200

    Initial Import

---
+++ send-invitations.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+src="$1"
+
+name=""
+email=""
+
+if [ ! -f ${src} ]; then
+	echo "Usage: $(basename $0) ALPSS.csv"
+	exit 1
+fi
+
+old_IFS=${IFS}
+IFS=","
+
+cat ${src} | while read -a array; do
+	email="${array[0]}"
+	name="${array[1]}"
+
+	tmpfile=$(mktemp)
+	sed -s "s/\$INVITEE/$name/" invitation.txt > $tmpfile
+
+	mutt -s "Invitation to ALPSS Conference" -c jthumshirn@suse.de "$name <$email>" < $tmpfile
+	rm $tmpfile
+done
+
+IFS=$old_IFS
